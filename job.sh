214d8fbb6daaf21ad4a94d665ac57c7a40e6da26
Susanna Di-Vita <sdivita@tikgpu10.ee.ethz.ch>
2024-12-29 18:12:38 +0100

    k

--- job.sh
+++ job.sh
@@ -37,7 +37,7 @@ cd ${DIRECTORY}
 
 # Execute training script with optimized parameters
 python train.py configs/original_default.yml \
-    --batch-size 512 \
+    --batch-size 768 \
     --num-workers 8 \
     --devices 4 \
     --log-dir /itet-stor/sdivita/net_scratch/shitong/ChemProjector/runs/original_training
