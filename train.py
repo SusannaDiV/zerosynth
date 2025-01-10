28712293f314c903d2387f55e06c5521fd8e5b45
Shitong Luo <luost26@gmail.com>
2025-01-10 05:21:06 -0500

    Added prefetch factor

--- train.py
+++ train.py
@@ -96,7 +96,6 @@ def main(
         config,
         batch_size=batch_size_per_process,
         num_workers=num_workers,
-        prefetch_factor=2,
         pin_memory=True,
         **config.data,
     )
